dd1062693c7eaab281eb7477114345be64bc6414
pc utfv <21307021@utfv.edu.mx>
2023-07-17 19:06:32 -0500

    17072023

---
+++ app/php/insertatr.php
@@ -0,0 +1,10 @@
+<?php
+
+error_reporting(E_ALL ^ E_DEPRECATED);
+header("content-type: text/html; charset=UTF-8");
+
+$variable1=123;
+$variable2="sandy";
+echo $variable1 . '<br>'. $variable2 ;
+
+?>
---
+++ respuesta.php
@@ -0,0 +1,13 @@
+<?php
+
+$correo= $_POST['correo'];
+$password= $_POST['password'];
+
+
+echo $correo;
+echo '<br>';
+echo $password;
+
+
+
+?>
